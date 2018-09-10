770d6558d8508d49f947d9abc85a8a306dc5992e
Tom Flídr <tomflidr@gmail.com>
2018-09-10 20:04:05 +0200

    form extension - all comments ok

--- src/MvcCore/Ext/Auths/Basics/IForm.php
+++ src/MvcCore/Ext/Auths/Basics/IForm.php
@@ -30,8 +30,8 @@ interface IForm
 
 	/**
 	 * Set form id, required to configure.
-	 * Form id is used to identify session data, error messages,
-	 * csrf tokens, html form attribute id value and much more.
+	 * Used to identify session data, error messages,
+	 * CSRF tokens, html form attribute id value and much more.
 	 * @requires
 	 * @param string $id
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
@@ -42,15 +42,18 @@ interface IForm
 	 * Set form submitting url value.
 	 * It could be relative or absolute, anything
 	 * to complete classic html form attribute `action`.
-	 * @requires
-	 * @param string $url
+	 * @param string|NULL $url
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
 	public function & SetAction ($url = NULL);
 
 	/**
-	 * Set form http submitting method.
-	 * `POST` by default.
+	 * Set form http submitting method.`POST` by default. 
+	 * Use `GET` only if form data contains only ASCII characters.
+	 * Possible values: `'POST' | 'GET'`
+	 * You can use constants:
+	 * - `\MvcCore\Ext\Forms\IForm::METHOD_POST`
+	 * - `\MvcCore\Ext\Forms\IForm::METHOD_GET`
 	 * @param string $method
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
@@ -68,63 +71,68 @@ interface IForm
 	public function & SetCssClasses ($cssClasses);
 
 	/**
-	 * Set success url string, relative or absolute, to specify, where
-	 * to redirect user after form submitted successfully.
-	 * It's not required to use `\MvcCore\Ext\Form` like this, but if you want to use method
-	 * `$form->SubmittedRedirect();` at the end of custom `Submit()` method implementation,
+	 * Set form success submit url string to redirect after, relative or absolute,
+	 * to specify, where to redirect user after form has been submitted successfully.
+	 * It's required to use `\MvcCore\Ext\Form` like this, if you want to use method
+	 * `$form->SubmittedRedirect();`, at the end of custom `Submit()` method implementation,
 	 * you need to specify at least success and error url strings.
-	 * @param string $url
+	 * @param string|NULL $url
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
 	public function & SetSuccessUrl ($url = NULL);
 
 	/**
-	 * Set error url string, relative or absolute, to specify, where
-	 * to redirect user after form not submitted successfully.
+	 * Set form error submit url string, relative or absolute, to specify,
+	 * where to redirect user after has not been submitted successfully.
 	 * It's not required to use `\MvcCore\Ext\Form` like this, but if you want to use method
 	 * `$form->SubmittedRedirect();` at the end of custom `Submit()` method implementation,
 	 * you need to specify at least success and error url strings.
-	 * @param string $url
+	 * @param string|NULL $url
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
 	public function & SetErrorUrl ($url = NULL);
 
 	/**
-	 * Set translator callable to translate everything visible in form.
-	 * Handler is necessary to design with first param to be a translation key,
-	 * second param to be a language code and hadler has to return translated string result.
-	 * This property is optional to configure, but if it is configured to any callable,
-	 * everything in form will be translated, except fields strictly defined to not translate.
-	 * Default value is `NULL`, it means no translation will be processed.
-	 * @param callable $handler
+	 * Set translator to translate field labels, options, placeholders and error messages.
+	 * Translator has to be `callable` (it could be `closure function` or `array`
+	 * with `classname/instance` and `method name` string). First argument
+	 * of `callable` has to be a translation key and second argument
+	 * has to be language string (`en`, `de` ...) to translate the key into.
+	 * Result of `callable` object has to be a string - translated key for called language.
+	 * @param callable|NULL $handler
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
 	public function & SetTranslator (callable $translator = NULL);
 
 	/**
-	 * Initialize the form, check if form is initialized or not and do it only once.
-	 * Check if any form id exists and initialize translation boolean for better field initializations.
-	 * This is template method. To define any fields in custom `\MvcCore\Ext\Form` class extension,
-	 * do it in `Init()` method and call `parent::Init();` as first line inside your custom `Init()` method.
-	 * @throws \RuntimeException No form id property defined.
-	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
+	 * Render whole `<form>` with all content into HTML string to display it.
+	 * - If form is not initialized, there is automaticly
+	 *   called `$form->Init();` method.
+	 * - If form is not pre-dispatched for rendering, there is
+	 *   automaticly called `$form->Predispatch();` method.
+	 * - Create new form view instance and set up the view with local
+	 *   context variables.
+	 * - Render form naturaly or by custom template.
+	 * - Clean session errors, because errors shoud be rendered
+	 *   only once, only when it's used and it's now - in this rendering process.
+	 * @return string
 	 */
 	public function Init ();
 
 	/**
-	 * Add configured form field instance.
-	 * @param \MvcCore\Ext\Forms\IField $field
+	 * Add multiple fully configured form field instances,
+	 * function have infinite params with new field instances.
+	 * @param \MvcCore\Ext\Forms\Field[]|\MvcCore\Ext\Forms\IField[] $fields,... Any `\MvcCore\Ext\Forms\IField` fully configured instance to add into form.
+	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
+	 */
+	public function & AddFields (/* ...$fields */);
+
+	/**
+	 * Add fully configured form field instance.
+	 * @param \MvcCore\Ext\Forms\Field|\MvcCore\Ext\Forms\IField $field
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
 	public function & AddField (\MvcCore\Ext\Forms\IField $field);
-
-	/**
-	 * Add multiple configured form field instances,
-	 * function have infinite params with new field instances.
-	 * @param \MvcCore\Ext\Forms\IField[] $fields,... Any `\MvcCore\Ext\Forms\IField` instance to add into form.
-	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
-	 */
-	public function & AddFields ();
 
 	/**
 	 * Process standard low level submit process.
@@ -142,33 +150,34 @@ interface IForm
 	public function Submit (array & $rawRequestParams = []);
 
 	/**
-	 * Clear all session records for this form by form id.
-	 * Data sended from last submit, any csrf tokens and any errors.
+	 * Clear form values to empty array and clear form values in form session namespace,
+	 * clear form errors to empty array and clear form errors in form session namespace and
+	 * clear form CSRF tokens clear CRSF tokens in form session namespace.
 	 * @return \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm
 	 */
 	public function & ClearSession ();
 
 	/**
 	 * Call this function in custom `\MvcCore\Ext\Form::Submit();` method implementation
-	 * at the end of custom `Submit()` method to redirect user by configured success/error/next
+	 * at the end of custom `Submit()` method to redirect user by configured success/error/prev/next
 	 * step url address into final place and store everything into session.
+	 * You can also to redirect form after submit by yourself.
 	 * @return void
 	 */
 	public function SubmittedRedirect ();
 
 	/**
-	 * Alias for method `\MvcCore\Ext\Form::Render();`.
-	 * @see `\MvcCore\Ext\Form::Render();`
+	 * Rendering process alias for `\MvcCore\Ext\Form::Render();`.
 	 * @return string
 	 */
 	public function __toString ();
 
 	/**
-	 * Render form into string to display it.
+	 * Render whole `<form>` with all content into HTML string to display it.
 	 * - If form is not initialized, there is automaticly
 	 *   called `$form->Init();` method.
-	 * - If form is not prepared for rendering, there is
-	 *   automaticly called `$form->prepareForRendering();` method.
+	 * - If form is not pre-dispatched for rendering, there is
+	 *   automaticly called `$form->Predispatch();` method.
 	 * - Create new form view instance and set up the view with local
 	 *   context variables.
 	 * - Render form naturaly or by custom template.
