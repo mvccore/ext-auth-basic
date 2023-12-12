e1790e2f235386128a4b1ae21f8619d1e93eb453
Tom Flídr <tomflidr@gmail.com>
2023-12-12 12:46:10 +0100

    release v5.2.5

--- src/MvcCore/Ext/Auths/Basic.php
+++ src/MvcCore/Ext/Auths/Basic.php
@@ -34,7 +34,7 @@ class Basic implements \MvcCore\Ext\Auths\IBasic {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.4';
+	const VERSION = '5.2.5';
 
 	use \MvcCore\Ext\Auths\Basic\PropsGettersSetters;
 	use \MvcCore\Ext\Auths\Basic\Handling;
